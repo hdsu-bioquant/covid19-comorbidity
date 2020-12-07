6104f0242321a44c2362b0eb51e34ec8d4711d6c
caramirezal <cramireza@ciencias.unam.mx>
2020-12-07 16:52:26 +0100

    Imputation and correlation in Chua data

--- figure_03.R
+++ figure_03.R
@@ -1,288 +1,180 @@
 ## Evaluation of the correlation of the colon organoids
+source('https://raw.githubusercontent.com/KlugerLab/ALRA/master/alra.R')
+source('https://raw.githubusercontent.com/KlugerLab/ALRA/master/alraSeurat2.R')
 library(Seurat)
 library(dplyr)
 library(ggplot2)
-library(ggrepel)
 library(viridis)
-library(dorothea)
-library(viper)
+library(ggpubr)
+library(ggrepel)
+library(scDoc)
+library(scImpute)
 
-proyect_dir <- '/Users/carlosramirez/sc/serine_proteases_regulation/'
-setwd(proyect_dir)
-
-###############################################################
-##                                                           ##
-##      Figure 4c. TF activities in scRNA-Seq using          ##
-##      Ileum organoid data.                                 ##
-##                                                           ##
-###############################################################
+#proyect_dir <- '/Users/carlosramirez/sc/serine_proteases_regulation/'
+project_dir <- '/media/ag-cherrmann/cramirez/covid19-comorbidity/'
+setwd(project_dir)
 
 
-## Getting SPRG
-uniprot_SPRG.filtered <- readRDS(
-    'data/serine_proteases_filtered.rds'
+## Downloading data
+url_lorenz <- 'https://ndownloader.figshare.com/files/22927382'
+lorenz_file <- 'data/Lorenz/covid_nbt_main.rds'
+if ( ! file.exists(lorenz_file)) {
+    download.file(url = url_lorenz, 
+                  destfile = lorenz_file)
+}
+
+## Reading Lorenz data
+lorenz <- readRDS('data/Lorenz/covid_nbt_main.rds')
+
+####################################################################################
+lorenz@meta.data$sample %>% table() %>% sum()
+
+lorenz@assays$RNA@counts %>%
+    saveRDS(file = 'data/Lorenz/lorenz_raw_counts.rds')
+
+
+lorenz.sp <- lapply(unique(lorenz@meta.data$sample), 
+                    function(x) subset(lorenz, sample == x))
+
+names(lorenz.sp) <- unique(lorenz@meta.data$sample)
+
+## Data imputation
+dir.create('data/tmp/')
+for ( s in names(lorenz.sp)) {
+    
+    tmp_file <- paste0('data/tmp/', s, '.rds')
+    saveRDS(lorenz.sp[s][[1]]@assays$RNA@counts,
+            tmp_file)
+    
+    seurat <- readRDS(tmp_file)
+    
+    tmp_dir <- paste0('data/tmp/', s)
+    dir.create(tmp_dir)
+    
+    scimpute(# full path to raw count matrix
+        count_path = tmp_file, 
+        infile = "rds",           # format of input file
+        outfile = "rds",          # format of output file
+        out_dir = tmp_dir,           # full path to output directory
+        labeled = FALSE,          # cell type labels not available
+        drop_thre = 0.5,          # threshold set on dropout probability
+        Kcluster = length(unique(lorenz.sp[s][[1]]@meta.data$celltype)),             # 2 cell subpopulations
+        ncores = 15) 
+    
+    file.remove(tmp_file)
+    
+}
+
+#######################################################################################
+## Integrating results
+
+## Getting output files names
+imputed.names <- list.files('data/tmp', 
+                            pattern = 'scimpute_count.rds', 
+                            full.names = TRUE)
+
+## Reading into R and transform to Seurat 
+imputed.matrices <- sapply(imputed.names, readRDS)
+lapply(imputed.matrices, function(x) x[1:3, 1:3])
+imputed.seurat <- sapply(imputed.matrices, function(x)
+    CreateSeuratObject(counts = x,
+                       project = 'COVID19',
+                       assay = 'RNA', 
+                       min.cells = 1, 
+                       min.features = 1)                 
 )
-SPRGs <- strsplit(uniprot_SPRG.filtered$Gene.names, 
-                  split = ' |/') %>%
-        unlist() %>% sort() %>% unique()
-
-load('~/sc/organoids/data/COVID19_July.rda')
-
-## Loading TF activities
-TF_act <- read.csv(
-        '~/sc/organoids/analysis/scenic/ileum_corrected/ileum_aucell.csv'
-)
-head(TF_act)
-dim(TF_act)
-
-## Getting serine protease gene expression
-DefaultAssay(Illeum_H_T) <- 'RNA'
-SPRG.df <- FetchData(Illeum_H_T, vars = SPRGs)
-dim(SPRG.df)
-
-## Checking rownames pairing
-any(rownames(SPRG.df) != TF_act$Cell)
-
-## Merging results
-SPRG.df <- SPRG.df[, sapply(SPRG.df, sum) > 0]
-ileum.cor <- cor(SPRG.df, select(TF_act, -Cell), 
-                 method = 'spearman')
-ileum.cor[1:5, 1:5]
-colnames(ileum.cor) <- gsub('\\.', '', 
-                            colnames(ileum.cor))
-dim(ileum.cor)
-
-## Calculating mean TF activities
-TFA_mean <- apply(select(TF_act, -Cell), 2, mean)
-names(TFA_mean) <-  gsub('\\.', '', names(TFA_mean))
-
-## tfa correlation vs gene expression in ileum
-spint2_cor_tfa <- ileum.cor['SPINT2', ]
-
-vars <- intersect(names(TFA_mean),
-                  names(spint2_cor_tfa))
+imputed.seurat
 
 
-ileum.tfa.df <- data.frame(
-    spint2_cor_tfa = spint2_cor_tfa[vars],
-    mean_TF_Activity = tfs.mean_exp[vars]
-)
-dim(ileum.tfa.df)
+rm(imputed.matrices)
 
-## Adding TMPRSS2 coregulation
-tmprss2_cor_tfa <- ileum.cor['TMPRSS2', ]
-ileum.tfa.df$'tmprss2_cor_tfa' <- tmprss2_cor_tfa[vars]
+## Integration
+seurat.merged <- merge(x=imputed.seurat[1][[1]], 
+                       y=imputed.seurat[2:length(imputed.seurat)])
+seurat.merged@meta.data %>% head
+seurat.merged.md <- seurat.merged@meta.data %>%
+    add_rownames('id')
 
-st14_cor_tfa <- ileum.cor['ST14', ]
-ileum.tfa.df$'st14_cor_tfa' <- st14_cor_tfa[vars]
+seurat.merged.md <- dplyr::select(seurat.merged.md, id)
+## merge to lorenz metadata
+lorenz.md <- lorenz@meta.data 
+lorenz.md <- add_rownames(lorenz.md, var = 'id')
+seurat.merged.md <- merge(lorenz.md, seurat.merged.md)
+dim(seurat.merged.md)
+dim(lorenz)
+## Checking file names pairing
+any(! colnames(seurat.merged) == seurat.merged.md$id )
+## Correcting order
+rownames(seurat.merged.md) <- seurat.merged.md$id
+seurat.merged.md <- seurat.merged.md[colnames(seurat.merged), ]
+any(! colnames(seurat.merged) == seurat.merged.md$id )
+seurat.merged@meta.data <- seurat.merged.md
 
-spint1_cor_tfa <- ileum.cor['SPINT1', ]
-ileum.tfa.df$'spint1_cor_tfa' <- spint1_cor_tfa[vars]
+## Normalize data
+seurat.merged <- NormalizeData(seurat.merged)
+seurat.merged <- ScaleData(seurat.merged)
 
+###########################################################################################
+## Visualisation of the results
 
-
-cor_th <- 0
-mean_th <- 0.5
-pdf('figures/tfa_cor_spint2_tmprss2_ileum_org.pdf',
-    height = 7, width = 8)
-ileum.tfa.df %>%
-        mutate(gene = rownames(ileum.tfa.df)) %>%
-        mutate(highlight = ifelse( mean_TF_Activity > mean_th,
-                                  TRUE, FALSE)) %>%
-        mutate(gene_label = ifelse(highlight == TRUE,
-                                   gene, '')) %>%
-        ggplot(aes(x=spint2_cor_tfa, 
-                   y=tmprss2_cor_tfa,
-                   colour=mean_TF_Activity,
-                   label = gene_label)) +
-                geom_point(aes(size=mean_TF_Activity)) +
-                geom_text_repel(colour='black') +
-                theme_bw() +
-                scale_color_viridis() +
-                theme(panel.grid = element_blank()) +
-                geom_hline(yintercept = cor_th, 
-                           linetype='dashed') +
-                geom_vline(xintercept = cor_th,
-                           linetype = 'dashed') +
-                ylab('Cor(TF activity, TMPRSS2)') +
-                xlab('Cor(TF activity, SPINT2)') +
-                ggtitle('Ileum organoid')
+pdf('figures/spint2_viral_cor_lorenz.pdf')
+seurat.merged %>%
+    FetchData(vars = c(colnames(seurat.merged@meta.data),
+                       'SPINT2', 'ACE2', 'SPINT1', 'TMPRSS2',
+                       'CDX2', "SARS-CoV-2-p", "SARS-CoV-2-m")) %>%
+    filter(severity != 'control') %>%
+    filter(`SARS-CoV-2-p` > 0 | `SARS-CoV-2-m` > 0) %>%
+    ggscatter(x = "SPINT2", y = "SARS-CoV-2-p", 
+              add = "reg.line",
+              palette = "jco",
+              color = 'steelblue', 
+              size = 3.5) +
+    stat_cor(label.x = 0.7, 
+             label.y = 2.5)
 dev.off()
 
+## Loading SPRGs gene names
+## Loading SPRGs
+uniprot_SPRG.filtered <- readRDS('data/serine_proteases_filtered.rds')
+SPRGs <- strsplit(uniprot_SPRG.filtered$Gene.names, split = ' |/') %>%
+    unlist() %>% sort() %>% unique()
+SPRGs
 
-pdf('figures/tfa_cor_spint1_st14_ileum_org.pdf',
-    height = 7, width = 8)
-ileum.tfa.df %>%
-    mutate(gene = rownames(ileum.tfa.df)) %>%
-    mutate(highlight = ifelse( mean_TF_Activity > mean_th,
-                               TRUE, FALSE)) %>%
-    mutate(gene_label = ifelse(highlight == TRUE,
-                               gene, '')) %>%
-    ggplot(aes(x=spint1_cor_tfa, 
-               y=st14_cor_tfa,
-               colour=mean_TF_Activity,
-               label = gene_label)) +
-    geom_point(aes(size=mean_TF_Activity)) +
-    geom_text_repel(colour='black') +
+sprgs.df <- FetchData(subset(seurat.merged,
+                             `SARS-CoV-2-p` > 0 | `SARS-CoV-2-m` > 0), 
+                      vars = c(SPRGs, "SARS-CoV-2-p"))
+sprgs.cor <- cor(sprgs.df)
+
+pdf('figures/SPRGs_cor_viral_lorenz.pdf',
+    height = 7, width = 7)
+sprgs.cor %>%
+    as.data.frame() %>%
+    dplyr::select(`SARS-CoV-2-p`) %>%
+    arrange(desc(abs(`SARS-CoV-2-p`))) %>%
+    add_rownames(var = 'gene') %>%
+    filter(gene != 'SARS-CoV-2-p') %>%
+    mutate(rank=1:n()) %>%
+    mutate(highlight=ifelse(rank < 20, 
+                            TRUE,
+                            FALSE)) %>%
+    mutate(gene_label=ifelse(highlight ==TRUE,
+                             gene, '')) %>%
+    mutate(positive=ifelse(`SARS-CoV-2-p` >= 0,
+                           'Posivitely correlated', 
+                           'Negatively correlated')) %>%
+    filter(!is.na(`SARS-CoV-2-p`)) %>%
+    ggplot(aes(x=rank, 
+               y=abs(`SARS-CoV-2-p`),
+               label =gene_label,
+               colour=positive)) +
+    geom_point() +
+    geom_text_repel(colour='black',
+                    size=5) +
     theme_bw() +
-    scale_color_viridis() +
     theme(panel.grid = element_blank()) +
-    geom_hline(yintercept = cor_th, 
-               linetype='dashed') +
-    geom_vline(xintercept = cor_th,
-               linetype = 'dashed')+
-    ylab('Cor(TF activity, ST14)') +
-    xlab('Cor(TF activity, SPINT1)') +
-    ggtitle('Ileum organoid')
+    xlab('Rank') +
+    ylab('Cor(SPINT2, viral P protein SARS-CoV2)') +
+    scale_color_manual(values = c('red',
+                                  'chartreuse4'))
 dev.off()
 
-
-###############################################################
-##                                                           ##
-##      Figure 4d. TF Binding to SPINT2 and TMRPSS2          ##
-##      as observed in ATAC-Seq data from Human Intes-       ##
-##      tinal organoids                                      ##
-##                                                           ##
-###############################################################
-
-## Loading TOBIAS results
-files <- list.files('~/sc/covid19-comorbidity/analysis/TOBIAS/tfbs_HIO/', 
-                    full.names = TRUE)
-tfbs.list <- lapply(files,
-                    function(x)
-                        read.table(x, header = TRUE))
-
-## Merging TFBS
-tfbs.df  <- do.call(rbind, tfbs.list)
-
-## Getting common TF binding to SPINT2 and TMPRSS2
-spint2.tfb <- filter(tfbs.df, gene_name == 'SPINT2') %>%
-    dplyr::select(TFBS_name) %>%
-    unlist() %>% unique()
-tmprss2.tfb <- filter(tfbs.df, gene_name == 'TMPRSS2') %>%
-    dplyr::select(TFBS_name) %>%
-    unlist() %>% unique()
-tfbs.common <- intersect(spint2.tfb, tmprss2.tfb)
-tfbs.common.df <- filter(tfbs.df,
-                         gene_name %in% c('SPINT2', 
-                                          'TMPRSS2')) %>%
-    filter(TFBS_name %in% tfbs.common) 
-
-## Filtering using most correlated genes in scRNA-Seq data
-## from Figure 4c results
-pos.cor <- ileum.tfa.df %>%
-            filter(spint2_cor_tfa > 0 & 
-                       tmprss2_cor_tfa > 0) %>%
-                rownames()
-pos.cor
-
-tfbs.common.df %>%
-    mutate(TFBS_name = gsub('_.*', '', TFBS_name)) %>%
-    filter(TFBS_name %in% pos.cor) %>%
-    group_by(TFBS_name, gene_name) %>%
-    summarise(max=max(TFBS_score)) %>%
-    mutate(TFBS_score=max) %>%
-    mutate(TFBS_score_2=TFBS_score) %>%
-    dplyr::select(TFBS_name, gene_name, TFBS_score, TFBS_score_2) %>%
-    write.table('~/sc/covid19-comorbidity/analysis/network_tobias.tsv',
-                sep = '\t',
-                quote = FALSE,
-                row.names = FALSE)
-
-tfs <- gsub('_.*', '', tfbs.common.df$TFBS_name)
-intersection <- intersect(pos.cor, 
-                          unique(tfs))
-
-cor_th <- 0
-mean_th <- 0.5
-pdf('figures/tfa_cor_spint2_tmprss2_ileum_org_v2.pdf',
-    height = 7, width = 8)
-ileum.tfa.df %>%
-    mutate(gene = rownames(ileum.tfa.df)) %>%
-    mutate(highlight = ifelse( gene %in% intersection,
-                               TRUE, FALSE)) %>%
-    mutate(gene_label = ifelse(highlight == TRUE,
-                               gene, '')) %>%
-    ggplot(aes(x=spint2_cor_tfa, 
-               y=tmprss2_cor_tfa,
-               colour=mean_TF_Activity,
-               label = gene_label)) +
-    geom_point(aes(size=mean_TF_Activity)) +
-    geom_text_repel(colour='black') +
-    theme_bw() +
-    scale_color_viridis() +
-    theme(panel.grid = element_blank()) +
-    geom_hline(yintercept = cor_th, 
-               linetype='dashed') +
-    geom_vline(xintercept = cor_th,
-               linetype = 'dashed') +
-    ylab('Cor(TF activity, TMPRSS2)') +
-    xlab('Cor(TF activity, SPINT2)') +
-    ggtitle('Ileum organoid')
-dev.off()
-
-###########################################################
-## Same analysis for ST14 and SPINT1
-
-## Getting common TF binding to SPINT2 and TMPRSS2
-spint1.tfb <- filter(tfbs.df, gene_name == 'SPINT1') %>%
-    dplyr::select(TFBS_name) %>%
-    unlist() %>% unique()
-st14.tfb <- filter(tfbs.df, gene_name == 'ST14') %>%
-    dplyr::select(TFBS_name) %>%
-    unlist() %>% unique()
-tfbs.common <- intersect(spint1.tfb, st14.tfb)
-tfbs.common.df <- filter(tfbs.df,
-                         gene_name %in% c('SPINT1', 
-                                          'ST14')) %>%
-    filter(TFBS_name %in% tfbs.common) 
-
-## Filtering using most correlated genes in scRNA-Seq data
-## from Figure 4c results
-pos.cor <- ileum.tfa.df %>%
-    filter(spint1_cor_tfa > 0 | 
-               st14_cor_tfa > 0) %>%
-    rownames()
-pos.cor
-
-
-tfbs.common.df %>%
-    mutate(TFBS_name = gsub('_.*', '', TFBS_name)) %>%
-    filter(TFBS_name %in% pos.cor) %>%
-    group_by(TFBS_name, gene_name) %>%
-    summarise(max=max(TFBS_score)) %>%
-    mutate(TFBS_score=max) %>%
-    mutate(TFBS_score_2=TFBS_score) %>%
-    dplyr::select(TFBS_name, gene_name, TFBS_score, TFBS_score_2) %>%
-    write.table('~/sc/covid19-comorbidity/analysis/network_tobias_st14.tsv',
-                sep = '\t',
-                quote = FALSE,
-                row.names = FALSE)
-
-tfs <- gsub('_.*', '', tfbs.common.df$TFBS_name)
-intersection <- intersect(pos.cor, 
-                          unique(tfs))
-intersection
-
-ileum.tfa.df %>%
-    mutate(gene = rownames(ileum.tfa.df)) %>%
-    mutate(highlight = ifelse( gene %in% intersection,
-                               TRUE, FALSE)) %>%
-    mutate(gene_label = ifelse(highlight == TRUE,
-                               gene, '')) %>%
-    ggplot(aes(x=spint1_cor_tfa, 
-               y=st14_cor_tfa,
-               colour=mean_TF_Activity,
-               label = gene_label)) +
-    geom_point(aes(size=mean_TF_Activity)) +
-    geom_text_repel(colour='black') +
-    theme_bw() +
-    scale_color_viridis() +
-    theme(panel.grid = element_blank()) +
-    geom_hline(yintercept = cor_th, 
-               linetype='dashed') +
-    geom_vline(xintercept = cor_th,
-               linetype = 'dashed') +
-    ylab('Cor(TF activity, TMPRSS2)') +
-    xlab('Cor(TF activity, SPINT2)') +
-    ggtitle('Ileum organoid')
