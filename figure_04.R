f3adf790ec85289a8910c0503434daba3ae47ad5
caramirezal <caramirezal@ciencias.unam.mx>
2020-12-25 20:34:06 +0100

    Script to reproduce figure 4

--- figure_04.R
+++ figure_04.R
@@ -1,312 +1,219 @@
-## GTEx exploration of the gene expression profiles of serine 
-## proteases
-library(cmapR)
+## Evaluation of the correlation of the colon organoids
+source('https://raw.githubusercontent.com/KlugerLab/ALRA/master/alra.R')
+source('https://raw.githubusercontent.com/KlugerLab/ALRA/master/alraSeurat2.R')
+library(Seurat)
 library(dplyr)
 library(ggplot2)
-library(ggpubr)
 library(viridis)
+library(ggpubr)
 library(ggrepel)
-library(DESeq2)
+library(scDoc)
+library(scImpute)
 
-## Setting paths
-setwd('/Users/carlosramirez/sc/serine_proteases_regulation/')
+#proyect_dir <- '/Users/carlosramirez/sc/serine_proteases_regulation/'
+project_dir <- '/media/ag-cherrmann/cramirez/covid19-comorbidity/'
+setwd(project_dir)
 
-url_file <- 'https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz'
-file_path <- 'data/gtex/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct'
+######################################################################
+##                                                                  ##
+##      Figure 4A. Correlation of SPRGs to viral load in patients   ##
+##      nasopharynx samples from COVID-19 patients                  ##
+##                                                                  ##
+######################################################################
 
-## Downloading 
-if ( ! file.exists(file_path) ){
-        download.file(url = url_file,
-                      destfile = file_path)
+## Downloading data
+url_lorenz <- 'https://ndownloader.figshare.com/files/22927382'
+lorenz_file <- 'data/Lorenz/covid_nbt_main.rds'
+if ( ! file.exists(lorenz_file)) {
+    download.file(url = url_lorenz, 
+                  destfile = lorenz_file)
 }
 
-#gtex <- parse_gctx(file_path)
-#dim(gtex@mat)
-#gtex@mat[1:5, 1:5]
-#saveRDS(gtex@mat, 'data/gtex/GTEx_tpm.mtx')
-#rm(gtex)
+## Reading Lorenz data
+lorenz <- readRDS('data/Lorenz/covid_nbt_main.rds')
 
-#gtex.mtx <- readRDS('data/gtex/GTEx_tpm.mtx')
-#gtex.mtx[1:5, 1:5]
+## Saving results to imput
+lorenz@assays$RNA@counts %>%
+    saveRDS(file = 'data/Lorenz/lorenz_raw_counts.rds')
 
-#############################################################
-## Gene annotation
-## Reading gene annotations
-gtf_url <- 'https://storage.googleapis.com/gtex_analysis_v8/reference/gencode.v26.GRCh38.genes.gtf'
-#download.file(gtf_url, destfile = 'data/gtex/gencode.v26.GRCh38.genes.gtf')
+lorenz.sp <- lapply(unique(lorenz@meta.data$sample), 
+                    function(x) subset(lorenz, sample == x))
+names(lorenz.sp) <- unique(lorenz@meta.data$sample)
 
-gtf <- read.table(file = gtf_url, sep = '\t')
-head(gtf)
-
-
-extract_pattern <- function(string_vec, pattern){
-        string_vec <- gsub(paste0('.*', pattern), '', string_vec)
-        string_vec <- gsub(' |;.*', '', string_vec)
-        
-        return(string_vec)
+## Data imputation
+dir.create('data/tmp/')
+for ( s in names(lorenz.sp)) {
+    
+    tmp_file <- paste0('data/tmp/', s, '.rds')
+    saveRDS(lorenz.sp[s][[1]]@assays$RNA@counts,
+            tmp_file)
+    
+    seurat <- readRDS(tmp_file)
+    
+    tmp_dir <- paste0('data/tmp/', s)
+    dir.create(tmp_dir)
+    
+    scimpute(# full path to raw count matrix
+        count_path = tmp_file, 
+        infile = "rds",           # format of input file
+        outfile = "rds",          # format of output file
+        out_dir = tmp_dir,           # full path to output directory
+        labeled = FALSE,          # cell type labels not available
+        drop_thre = 0.5,          # threshold set on dropout probability
+        Kcluster = length(unique(lorenz.sp[s][[1]]@meta.data$celltype)),             # 2 cell subpopulations
+        ncores = 15) 
+    
+    file.remove(tmp_file)
+    
 }
 
-## Processing GTEx gene annotations 
-gene_id <- extract_pattern(gtf$V9, 'gene_id')
-gene_name <- extract_pattern(gtf$V9, 'gene_name')
+## Integrating imputed results
 
-gene_anns <- data.frame(gene_id=gene_id,
-                        gene_name=gene_name)
-gene_anns <- gene_anns[!duplicated(gene_anns), ]
-rownames(gene_anns) <- gene_anns$gene_id
+## Getting output files names
+imputed.names <- list.files('data/tmp', 
+                            pattern = 'scimpute_count.rds', 
+                            full.names = TRUE)
 
-head(gene_anns)
-
-## Cleanning gene names
-#gtex.mtx <- gtex.mtx[rownames(gtex.mtx) %in% 
-#                             rownames(gene_anns), ] 
-#gtex.mtx <- gtex.mtx[rownames(gene_anns), ]
-#rownames(gtex.mtx) <- gene_anns$gene_name
-#gtex.mtx[1:5, 1:5]
-
-#saveRDS(gtex.mtx, 'data/gtex/GTEx_tpm.mtx')
-###########################################################
-gtex.mtx <- readRDS('data/gtex/GTEx_tpm.mtx')
-
-## Sample annotations
-gtex.ann <- read.table(
-        'https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt',
-        header = TRUE, sep = '\t',
-        quote = ''
+## Reading into R and transform to Seurat 
+imputed.matrices <- sapply(imputed.names, readRDS)
+lapply(imputed.matrices, function(x) x[1:3, 1:3])
+imputed.seurat <- sapply(imputed.matrices, function(x)
+    CreateSeuratObject(counts = x,
+                       project = 'COVID19',
+                       assay = 'RNA', 
+                       min.cells = 1, 
+                       min.features = 1)                 
 )
-dim(gtex.ann)
-gtex.ann[1:5, 1:5]
+imputed.seurat
+rm(imputed.matrices)
 
-#############################################################
-## Subsetting to serine proteases related genes and TFs
+## Integration
+seurat.merged <- merge(x=imputed.seurat[1][[1]], 
+                       y=imputed.seurat[2:length(imputed.seurat)])
+seurat.merged@meta.data %>% head
+seurat.merged.md <- seurat.merged@meta.data %>%
+    add_rownames('id')
 
-## SPRGs
+seurat.merged.md <- dplyr::select(seurat.merged.md, id)
+## merge to lorenz metadata
+lorenz.md <- lorenz@meta.data 
+lorenz.md <- add_rownames(lorenz.md, var = 'id')
+seurat.merged.md <- merge(lorenz.md, seurat.merged.md)
+dim(seurat.merged.md)
+dim(lorenz)
+## Checking file names pairing
+any(! colnames(seurat.merged) == seurat.merged.md$id )
+## Correcting order
+rownames(seurat.merged.md) <- seurat.merged.md$id
+seurat.merged.md <- seurat.merged.md[colnames(seurat.merged), ]
+any(! colnames(seurat.merged) == seurat.merged.md$id )
+seurat.merged@meta.data <- seurat.merged.md
+
+## Normalize data
+seurat.merged <- NormalizeData(seurat.merged)
+seurat.merged <- ScaleData(seurat.merged)
+
+## Visualisation of the results
+
+## Loading SPRGs
 uniprot_SPRG.filtered <- readRDS('data/serine_proteases_filtered.rds')
 SPRGs <- strsplit(uniprot_SPRG.filtered$Gene.names, split = ' |/') %>%
-        unlist() %>% sort() %>% unique()
+    unlist() %>% sort() %>% unique()
+SPRGs
 
-## Downloading list of TFs
-tfs_url <- 'https://raw.githubusercontent.com/aertslab/pySCENIC/master/resources/hs_hgnc_curated_tfs.txt'
-tfs <- readLines(tfs_url)
+sprgs.df <- FetchData(subset(seurat.merged,
+                             `SARS-CoV-2-p` > 0 | `SARS-CoV-2-m` > 0), 
+                      vars = c(SPRGs, "SARS-CoV-2-p"))
+sprgs.cor <- cor(sprgs.df)
 
-## selected markers
-markers <- c('ACE2', 'CDX1', 'CDX2', 'HNF4A')
-
-genes <- c(SPRGs, tfs, markers)
-
-## extracting gene expression
-gtex.df <- t(gtex.mtx[rownames(gtex.mtx) %in% genes, ])
-#rm(gtex.mtx)
-
-## annotation by cell type
-unique(gtex.ann$SMTS)
-rownames(gtex.ann) <- gtex.ann$SAMPID
-
-anns <- gtex.ann[rownames(gtex.ann) %in%
-                         rownames(gtex.df), ]
-## test gene expression and annotations match
-any(rownames(anns) != rownames(gtex.df))
-
-## merge ge and annotations
-ge.ann <- cbind(gtex.df, anns)
-ge.ann <- ge.ann[, ! duplicated(colnames(ge.ann))]
-
-
-########################################################
-## Enterocytes analysis
-ent.gtex <- dplyr::filter(ge.ann, SMTS == "Small Intestine")
-
-sprs.cor <- cor(ent.gtex[, colnames(ent.gtex) %in% SPRGs], 
-                method = 'spearman')
-diag(sprs.cor) <- 0
-
-#######################################################
-##                                                   ##
-##      Heatmap of highly correlated SPRGs           ##
-##                                                   ##
-#######################################################
-## SPRGs
-pdf('figures/SPRG_correlation_ileum_GTEx.pdf',
-    width = 16, height = 24)
-pheatmap::pheatmap(sprs.cor, color = viridis(20))
+pdf('figures/SPRGs_cor_viral_lorenz.pdf',
+    height = 7, width = 7)
+sprgs.cor %>%
+    as.data.frame() %>%
+    dplyr::select(`SARS-CoV-2-p`) %>%
+    arrange(desc(abs(`SARS-CoV-2-p`))) %>%
+    add_rownames(var = 'gene') %>%
+    filter(gene != 'SARS-CoV-2-p') %>%
+    mutate(rank=1:n()) %>%
+    mutate(highlight=ifelse(rank < 20, 
+                            TRUE,
+                            FALSE)) %>%
+    mutate(gene_label=ifelse(highlight ==TRUE,
+                             gene, '')) %>%
+    mutate(positive=ifelse(`SARS-CoV-2-p` >= 0,
+                           'Posivitely correlated', 
+                           'Negatively correlated')) %>%
+    filter(!is.na(`SARS-CoV-2-p`)) %>%
+    ggplot(aes(x=rank, 
+               y=abs(`SARS-CoV-2-p`),
+               label =gene_label,
+               colour=positive)) +
+    geom_point() +
+    geom_text_repel(colour='black',
+                    size=5) +
+    theme_bw() +
+    theme(panel.grid = element_blank()) +
+    xlab('Rank') +
+    ylab('Cor(SPINT2, viral P protein SARS-CoV2)') +
+    scale_color_manual(values = c('red',
+                                  'chartreuse4'))
 dev.off()
 
-###########################################################
-## SPINT2 correlation
+######################################################################
+##                                                                  ##
+##      Figure 4B. ACE2, SPINT2 and TMPRSS2 gene expression in      ##
+##      nasopharynx samples from COVID-19 patients                  ##
+##                                                                  ##
+######################################################################
 
-## TFs 
-tf.cor <- cor(ent.gtex[, colnames(ent.gtex) %in% 
-                               c(tfs, 
-                                 'SPINT2', 
-                                 'TMPRSS2')], 
-              method = 'spearman')
-diag(tf.cor) <- 0
+## Mild vs Severe scRNASeq Lorenz et al, 2020 data
+naso <- readRDS('~/Downloads/12436517/covid_nbt_main.rds')
+naso
 
-spint2.cor.df <- data.frame(spint2_correlation=tf.cor[, 'SPINT2'])
-head(spint2.cor.df)
+secretorybyseverity <- DotPlot(subset(naso, 
+                                      celltype == 'Secretory' &
+                                          severity != 'control'), 
+                               group.by = 'severity', 
+                               features = markers, 
+                               dot.min = 0.1, 
+                               col.min = 0, 
+                               dot.scale = 35) +
+    scale_colour_viridis() +
+    theme_bw(base_size = 9) + 
+    theme(panel.grid = element_blank(), 
+          axis.text = element_text(colour="black", size=14), 
+          axis.line = element_blank(), 
+          text = element_text(family = 'Helvetica')) +
+    xlab('') + ylab('') + coord_flip()
 
-mean.df <- data.frame(mean_val=sapply(ent.gtex, 
-                                      mean, 
-                                      na.rm=TRUE))
-head(mean.df)
-
-vars <- intersect(rownames(spint2.cor.df), rownames(mean.df))
-spint2.cor.df <- as.data.frame(spint2.cor.df[vars,])
-rownames(spint2.cor.df) <- vars
-mean.df <- mean.df[vars,]
-## check rownames are equal
-any(rownames(spint2.cor.df)!=rownames(mean.df))
-df <- cbind(spint2.cor.df, mean.df)
-rownames(df) <- rownames(spint2.cor.df)
-colnames(df) <- c('correlation', 'mean')
-head(df)
-
-## Thresholds
-cor_thr <- 0.8
-mean_thr <- 2.5
-xlims <- 1.05
-
-df <- df %>%
-        as.data.frame() %>%
-        mutate(gene=rownames(df)) %>%
-        mutate(highlight=ifelse(log(mean+1) > mean_thr & 
-                                        ( correlation > 0.8  |
-                                                  correlation < - 0.85),
-                                TRUE, FALSE)) %>%
-        mutate(gene_label=ifelse(highlight==TRUE, 
-                                 gene, ''))
-
-cor_genes.ileum <- filter(df, highlight == TRUE &
-                                  correlation > 0 ) %>%
-                        select(gene) %>%
-                        unlist() %>%
-                        unname()
-                
-        
-pdf('figures/spint2_tfs_correlation_ileum_gtex.pdf',
-    width = 9, height = 7)
-spint2.plot <- df %>%
-        ggplot(aes(x=correlation, y=log(mean+1),
-                   colour=highlight, 
-                   label=gene_label)) +
-        geom_point() +
-        geom_vline(xintercept = c(-cor_thr, cor_thr),
-                   linetype = 'dashed',
-                   colour='gray5') +
-        geom_hline(yintercept = mean_thr,
-                   linetype = 'dashed',
-                   colour='gray5') +
-        geom_text_repel() +
-        scale_color_manual(values = c('black', 'red')) +
-        theme_bw() +
-        theme(legend.position = 'none',
-              panel.grid = element_blank()) +
-        xlim(-xlims, xlims) +
-        xlab('Spearman Rho values') +
-        ylab('Log(mean TPM + 1)')
-spint2.plot
-dev.off()
-
-##############################################################
-## TMPRSS2
-tmprss2.cor.df <- data.frame(tmprss2.df_correlation=tf.cor[, 'TMPRSS2'])
-head(tmprss2.cor.df)
-
-mean.df <- data.frame(mean_val=sapply(ent.gtex, 
-                                      mean, 
-                                      na.rm=TRUE))
-head(mean.df)
-
-vars <- intersect(rownames(tmprss2.cor.df), rownames(mean.df))
-tmprss2.cor.df <- as.data.frame(tmprss2.cor.df[vars,])
-rownames(tmprss2.cor.df) <- vars
-mean.df <- mean.df[vars,]
-## check rownames are equal
-any(rownames(tmprss2.cor.df)!=rownames(mean.df))
-tmprss2.df <- cbind(tmprss2.cor.df, mean.df)
-rownames(tmprss2.df) <- rownames(tmprss2.cor.df)
-colnames(tmprss2.df) <- c('correlation', 'mean')
-head(tmprss2.df)
-
-tmprss2.df <- tmprss2.df %>%
-        as.data.frame() %>%
-        mutate(gene=rownames(tmprss2.df)) %>%
-        mutate(highlight=ifelse(log(mean+1) > mean_thr & 
-                                        ( correlation > 0.8  |
-                                                  correlation < - 0.85),
-                                TRUE, FALSE)) %>%
-        mutate(gene_label=ifelse(highlight==TRUE, 
-                                 gene, '')) %>%
-        filter( ! gene %in% c('SPINT2', 'TMPRSS2'))
-head(tmprss2.df)
-
-cor_tmprss2_genes.ileum <- filter(tmprss2.df, 
-                                  highlight == TRUE &
-                                  correlation > 0 ) %>%
-        select(gene) %>%
-        unlist() %>%
-        unname()
-
-
-cor_thr <- 0.8
-mean_thr <- 2.5
-xlims <- 1.05
-pdf('figures/tmprss2_tfs_correlation_ileum_gtex.pdf',
-    width = 9, height = 7)
-tmprss2.plot <- tmprss2.df %>%
-        ggplot(aes(x=correlation, y=log(mean+1),
-                   colour=highlight, 
-                   label=gene_label)) +
-        geom_point() +
-        geom_vline(xintercept = c(-cor_thr, cor_thr),
-                   linetype = 'dashed',
-                   colour='gray5') +
-        geom_hline(yintercept = mean_thr,
-                   linetype = 'dashed',
-                   colour='gray5') +
-        geom_text_repel() +
-        scale_color_manual(values = c('black', 'red')) +
-        theme_bw() +
-        theme(legend.position = 'none',
-              panel.grid = element_blank()) +
-        xlim(-xlims, xlims) +
-        xlab('Spearman Rho values') +
-        ylab('Log(mean TPM + 1)')
-tmprss2.plot
-dev.off()
-
-###############################################################
-
-plot <- df %>%
-        mutate(highlight = ( highlight == TRUE & 
-                                     gene_label %in% cor_tmprss2_genes.ileum )) %>%
-        ggplot(aes(x=correlation, y=log(mean+1),
-                   colour=highlight, 
-                   label=gene_label)) +
-        geom_point() +
-        geom_vline(xintercept = c(-cor_thr, cor_thr),
-                   linetype = 'dashed',
-                   colour='gray5') +
-        geom_hline(yintercept = mean_thr,
-                   linetype = 'dashed',
-                   colour='gray5') +
-        geom_text_repel() +
-        scale_color_manual(values = c('black', 'red')) +
-        theme_bw() +
-        theme(legend.position = 'none',
-              panel.grid = element_blank()) +
-        xlim(-xlims, xlims) +
-        xlab('Spearman Rho values') +
-        ylab('Log(mean TPM + 1)')
-plot
-
-pdf('figures/tfs_correlation_ileum_gtex.pdf',
+pdf('/Users/carlosramirez/sc/sars-cov2/figures/figure_02_00_mild_vs_severe_lorenz.pdf', 
     width = 15)
-gridExtra::grid.arrange(
-        spint2.plot, 
-        tmprss2.plot, 
-        ncol = 2
-)
-plot
+secretorybyseverity
 dev.off()
 
+######################################################################
+##                                                                  ##
+##      Figure 4C. ACE2, SPINT2 and TMPRSS2 genes expression        ##
+##      in PBMC from COVID-19 patients                              ##
+##                                                                  ##
+######################################################################
+
+## Mild vs severe scRNA-Seq Blish 
+pbmc <- readRDS('~/Downloads/blish_covid.seu.rds')
+pbmc <- subset(pbmc, Admission.level %in% c('ICU', 'Floor'))
+
+pbmc.plot.subset <- DotPlot(subset(pbmc, cell.type %in% c('DC', 'pDC',
+                                                          'SC & Eosinophil')), 
+                            group.by = 'Admission.level', 
+                            features = markers, 
+                            dot.min = 0.1, 
+                            col.min = 0, 
+                            dot.scale = 50) +
+    scale_colour_viridis() +
+    theme_bw(base_size = 9) + 
+    theme(panel.grid = element_blank(), 
+          axis.text = element_text(colour="black", size=14), 
+          axis.line = element_blank(), 
+          text = element_text(family = 'Helvetica')) +
+    xlab('') + ylab('') + 
+    ggtitle('DCs + pDC + SC & Eosinophil') + coord_flip()
